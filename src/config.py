1751247b5f4199d1e6cc7cc09099af1304096f53
Minwook Yi <grs0416@gmail.com>
2024-07-07 18:58:53 +0900

    Fix: 오타수정

--- src/config.py
+++ src/config.py
@@ -3,8 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-
 TELEGRAM_TOKEN = os.environ.get('TELEGRAM_TOKEN')
 CHAT_ID = os.environ.get('CHAT_ID')
 PURCHASE_PAGE_URL = os.environ.get('PURCHASE_PAGE_URL')
-CHROME_DRIVER_PATH = os.environ.get("CHROME_DIRVER_PATH") # 실제 ChromeDriver의 경로로 변경
+CHROME_DRIVER_PATH = os.environ.get("CHROME_DRIVER_PATH") # 실제 ChromeDriver의 경로로 변경
